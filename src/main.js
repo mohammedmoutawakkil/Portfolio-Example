1a8ddead0b9f60ef0361c0a72a6dfa5cb822de44
mohammedmoutawakkil <mohamedmoutawakkil40@gmail.com>
2022-03-18 17:59:32 +0100

    add Logo and back to top button

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import './index.css'
 //Routes
 import router from './router'
 import store from './store'
-import BackToTop from 'vue-backtotop'
 
 //fontawesome component
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -16,4 +15,4 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fas, fab);
-createApp(App).use(router).use(store).use(BackToTop).component('fa', FontAwesomeIcon).mount('#app')
+createApp(App).use(router).use(store).component('fa', FontAwesomeIcon).mount('#app')
