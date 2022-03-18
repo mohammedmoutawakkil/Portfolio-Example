98b6418cd0c5e31dcc38c4989256a160ce6e866e
mohammedmoutawakkil <mohamedmoutawakkil40@gmail.com>
2022-03-18 18:08:19 +0100

    done

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,35 +2,35 @@ const projects = [{
         id: 1,
         title: 'Laureats',
         category: 'Web Application',
-        img: '/src/assets/images/projects/web-project-1.png',
+        img: '/src/assets/images/web-project-1.png',
         link: 'http://laureats.fsjes-umi.ac.ma/'
     },
     {
         id: 2,
         title: 'MK Market',
         category: 'Desktop Application/OpenSource',
-        img: '/src/assets/images/projects/desktop-project-1.png',
+        img: '/src/assets/images/desktop-project-1.png',
         link: 'https://github.com/mohammedmoutawakkil/MKMarket'
     },
     {
         id: 3,
         title: 'MK Template',
         category: 'UI/OpenSource',
-        img: '/src/assets/images/projects/ui-project-1.png',
+        img: '/src/assets/images/ui-project-1.png',
         link: 'https://mktemplate.vercel.app/'
     },
     {
         id: 4,
         title: 'MK Coffee',
         category: 'UI/OpenSource',
-        img: '/src/assets/images/projects/ui-project-2.png',
+        img: '/src/assets/images/ui-project-2.png',
         link: 'https://mkcoffee.vercel.app/'
     },
     {
         id: 5,
         title: 'Easy Booking',
         category: 'Web Application',
-        img: '/src/assets/images/projects/web-project-2.png',
+        img: '/src/assets/images/web-project-2.png',
         link: 'javascript:void(0)'
     },
 ];
