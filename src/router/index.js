f665d805e99deb14836022237eec5941b22d79e8
mohamed moutawakkil <mohammedmoutawakkil>
2022-07-14 10:27:39 +0100

    Not Found Route

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,37 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from '/src/views/Home.vue';
-import About from '/src/views/About.vue';
-import Contact from '/src/views/Contact.vue';
-import Projects from '/src/views/Projects.vue';
-const routes = [{
-    path: '/',
-    name: 'Home',
-    component: Home,
-}, {
-    path: '/About',
-    name: 'About',
-    component: About,
-}, {
-    path: '/ContactUS',
-    name: 'ContactUS',
-    component: Contact,
-}, {
-    path: '/Projects',
-    name: 'Projects',
-    component: Projects,
-}, ];
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '/src/views/Home.vue'
+import About from '/src/views/About.vue'
+import Contact from '/src/views/Contact.vue'
+import Projects from '/src/views/Projects.vue'
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+    },
+    {
+        path: '/contactus',
+        name: 'ContactUS',
+        component: Contact,
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        redirect: { name: 'Home'}
+    },
+    {
+        path: '/projects',
+        name: 'Projects',
+        component: Projects,
+    },
+]
 const router = createRouter({
     history: createWebHistory(),
-    routes
-});
+    routes,
+})
 export default router
